177d8b80705dac272cdee2e326a48191bbb0a19c
Tien0404 <153936432+Tien0404@users.noreply.github.com>
2024-12-08 23:50:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section");
+
+    sections.forEach((section, index) => {
+        section.style.animationDelay = `${index * 0.2}s`;
+    });
+});
